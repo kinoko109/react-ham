ef2983d349a682545441321658b6ced90a832201
松岡龍紀 <tatsuaki_matsuoka@bita.jp>
2020-04-12 18:41:18 +0900

    update：counter実装

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const App = () => {
+  return <Counter></Counter>
+}
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    }
+  }
+
+  handlePlus() {
+    this.setState({count: this.state.count + 1})
+  }
+
+  handleMinus() {
+    this.setState({count: this.state.count - 1})
+  }
+
+  render() {
+    return(
+      <>
+        <div>counter:{ this.state.count }</div>
+        <button onClick={this.handlePlus.bind(this)}>+</button>
+        <button onClick={this.handleMinus.bind(this)}>-</button>
+      </>
+    );
+  }
 }
 
 export default App;
